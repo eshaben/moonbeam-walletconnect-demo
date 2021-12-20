ba7e6291c951a6b67dd8b0c4dc0f0b4319bed1dc
eshaben <eshaben@icloud.com>
2021-12-20 12:51:20 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import WalletConnect, { CLIENT_EVENTS } from "@walletconnect/client";
+import QRCodeModal from "@walletconnect/qrcode-modal";
+import React from 'react';
+import { ethers } from "ethers";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Modal from "./components/Modal";
+
+const INITIAL_STATE = {
+  connector: null,
+  chainID: 1287,
+  accounts: [],
+  address: null
+}
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {...INITIAL_STATE};
+  }
+
+  connect = async () => {
+    console.log("clicked")
+
+    const connector = new WalletConnect({
+      rpc: { [this.state.chainId]: 'https://rpc.testnet.moonbeam.network' },
+      bridge: 'https://bridge.walletconnect.org',
+      qrcodeModal: true
+    })
+
+    const {accounts, chainId} = await connector.connect()
+
+    QRCodeModal.open("http://localhost:3000", () => {
+      console.log("opened")
+    })
+
+    console.log("connected: ", accounts, chainId)
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Moonbeam/Wallet Connect Demo App
+          </p>
+          <button
+            className="App-link"
+            target="_blank"
+            onClick={this.connect}
+          >
+            Connect to WalletConnect
+          </button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
